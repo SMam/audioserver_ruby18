7d49224769208c28d05d1acf1301c3f2a86bdebe
S Mamiya <mamiya@zotac.ma.jp>
2011-01-12 23:40:18 +0900

    rails new audiogram

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Audioserver::Application.config.secret_token = '72b4c69ec9c671bedb1d9ac61d8fef8ff828ff12623e9debf81b8b175f9ea3b961c2edb6797fe807cb8babbd7a5b9a9acefb4771a7828871f3b870a124c94c5a'
